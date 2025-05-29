3116195e85701cdcda04d85eea06af1284c69a07
grancesco <francesco.galletti@lnf.infn.it>
2025-05-29 13:10:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
-
+console.log("main.js caricato!");
 // Creazione scena, camera e renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
